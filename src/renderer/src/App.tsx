96a8e1e1d75a8c99851462c5496630e74033fb1e
daichi2mori <daichi2mori@gmail.com>
2024-08-10 23:50:35 +0900

    remove: comments

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -11,28 +11,6 @@ function App(): JSX.Element {
   }
 
   const handleSaveImages = async (): Promise<void> => {
-    // if (!Array.isArray(imgUrls)) {
-    //   alert('画像URLが取得されていません')
-    //   return
-    // }
-
-    // for (let i = 0; i < imgUrls.length; i++) {
-    //   try {
-    //     const response = await fetch(imgUrls[i])
-    //     const blob = await response.blob()
-    //     const urlObject = window.URL.createObjectURL(blob)
-    //     const a = document.createElement('a')
-    //     a.href = urlObject
-    //     a.download = `image-${i}.jpg`
-    //     document.body.appendChild(a)
-    //     a.click()
-    //     document.body.removeChild(a)
-    //     window.URL.revokeObjectURL(urlObject)
-    //   } catch (error) {
-    //     setMsg('画像の保存に失敗しました')
-    //   }
-    // }
-
     const result = await window.imgDI.saveImgs()
 
     switch (result) {
